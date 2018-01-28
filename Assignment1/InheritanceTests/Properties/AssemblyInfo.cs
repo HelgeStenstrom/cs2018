540be04327b32daf2f661cc4963d5e79bf9540a1
Helge Stenström <h.stenstrom@gmail.com>
2018-01-28 11:54:08 +0100

    Inheritance: visa problem med ofullständigt objekt.

---
+++ Assignment1/InheritanceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InheritanceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("InheritanceTests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6e44c2d-41af-4524-95e1-af046bc11af3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignment1/InheritanceTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Inheritance;
+
+namespace InheritanceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PersonWithInvalidAddress()
+        {
+            Person person = new Person("ett", "två");
+            string address = person.Address.ToString();
+            //Console.WriteLine(address);
+            //Assert.AreEqual("hej", address.ToString());
+        }
+    }
+}

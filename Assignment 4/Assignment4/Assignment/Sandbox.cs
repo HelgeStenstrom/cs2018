45885fe7c4aa9e4bfdd3755eb14a3e636df21b6a
Helge Stentröm <h.stenstrom@gmail.com>
2018-03-31 17:32:32 +0200

    Försöker få allt till namespace Assignment
    Men fortfarande tycker Rider att det ska vara Assignment2.

--- Assignment 4/Assignment4/Assignment/Sandbox.cs
+++ Assignment 4/Assignment4/Assignment/Sandbox.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Assignment4
+namespace Assignment
 {
     /// <summary>
     /// My playing ground. Therefore not commented.

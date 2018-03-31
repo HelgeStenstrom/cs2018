3da8dd9e84b54a64781b6d528071f2d9dbaa8727
Helge Stenström <h.stenstrom@gmail.com>
2018-03-31 22:04:25 +0200

    Försöker rensa bort Assignment2, men är inte klar.

--- Assignment 4/Assignment4/Assignment/Properties/Resources.Designer.cs
+++ Assignment 4/Assignment4/Assignment/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace Assignment.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace Assignment.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Assignment2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Assignment.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

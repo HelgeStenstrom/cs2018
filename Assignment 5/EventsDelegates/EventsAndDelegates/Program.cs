32bcb038ae796adfb78f20b5ed063982aa2f509a
Helge Stentröm <h.stenstrom@gmail.com>
2018-04-08 23:05:52 +0200

    Skrivit in delar av kod från YouTube-demo

---
+++ Assignment 5/EventsDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video(){ Title = "Video 1"};
+            var videoEncoder = new VideoEncoder();
+            
+            videoEncoder.Encode(video); 
+            
+        }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs e)
+        {
+            Console.WriteLine("Mailservice: Sending an email...");
+        }
+    }
+}

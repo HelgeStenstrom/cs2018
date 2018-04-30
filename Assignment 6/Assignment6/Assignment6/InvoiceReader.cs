4787f1a4f29a79a1328215a978510c4831ebceb5
Helge Stenström <helge.stenstrom@ericsson.com>
2018-04-30 12:32:08 +0200

    Assignment 6. InvoiceReader

---
+++ Assignment 6/Assignment6/Assignment6/InvoiceReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    public class InvoiceReader
+    {
+        string fileName;
+
+        public InvoiceReader(string fileName)
+        {
+            this.fileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
+        }
+    }
+}
---
+++ Assignment 6/Assignment6/Assignment6Tests/InvoiceReaderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6.Tests
+{
+    [TestClass()]
+    public class InvoiceReaderTests
+    {
+        [TestMethod()]
+        public void InvoiceReaderTest()
+        {
+            string fn = "InvoiceDemo1.txt";
+            var x = new InvoiceReader(fn);
+
+
+
+            Assert.Fail("Test not done");
+        }
+    }
+}

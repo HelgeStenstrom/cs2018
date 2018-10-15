5944c85e80722bfb31cd79324fc084202e013430
Helge Stentröm <h.stenstrom@gmail.com>
2018-10-15 18:21:53 +0200

    Sandbox, för bortglömt syfte

---
+++ Assignment 5/EventsDelegatesConsole/EventsDelegatesConsole/Sandbox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace EventsDelegatesConsole
+{
+    public class Sandbox
+    {
+        private double c;
+
+        public T DoSomething<T>(T obj)
+        {
+            return obj;
+        }
+
+        void f()
+        {
+            int a;
+            a = DoSomething<int>((int) c);
+            a = DoSomething((int) c);
+
+            string b = DoSomething<string>(c.ToString());
+
+            
+            c = DoSomething(Math.PI);
+            c = DoSomething<double>(Math.PI);
+        }
+    }
+    
+    
+}
